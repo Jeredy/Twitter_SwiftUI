cdaf6f51baf79f34d723e523d5bcab2847fe8a78
Andre Rodrigues <andrecomerciale@gmail.com>
2022-07-28 22:42:44 -0300

    Initial Commit

---
+++ Shared/Twitter_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Twitter_SwiftUIApp.swift
+//  Shared
+//
+//  Created by André Almeida on 2022-07-28.
+//
+
+import SwiftUI
+
+@main
+struct Twitter_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
